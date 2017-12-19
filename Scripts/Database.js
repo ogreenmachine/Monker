52126e2a1a6243c2a7f196830183d5a46d79a493
Adam Orr <aorr@smg.com>
2017-12-19 09:16:39 -0600

    Uploading recent changes

--- Scripts/Database.js
+++ Scripts/Database.js
@@ -2,18 +2,39 @@ var db = openDatabase('monkerDB', '1.0', 'Test DB', 2 * 1024 * 1024);
 
 db.transaction(function (tx) {
     tx.executeSql('CREATE TABLE IF NOT EXISTS weights (weightClass unique, us,them)');
-    tx.executeSql('INSERT INTO weights VALUES("103",0,0)');
+    tx.executeSql('INSERT INTO weights VALUES("106",0,0)');
     tx.executeSql('INSERT INTO weights VALUES("113",0,0)');
     tx.executeSql('INSERT INTO weights VALUES("120",0,0)');
     tx.executeSql('INSERT INTO weights VALUES("126",0,0)');
     tx.executeSql('INSERT INTO weights VALUES("132",0,0)');
     tx.executeSql('INSERT INTO weights VALUES("138",0,0)');
     tx.executeSql('INSERT INTO weights VALUES("145",0,0)');
-    tx.executeSql('INSERT INTO weights VALUES("154",0,0)');
+    tx.executeSql('INSERT INTO weights VALUES("152",0,0)');
     tx.executeSql('INSERT INTO weights VALUES("160",0,0)');
     tx.executeSql('INSERT INTO weights VALUES("170",0,0)');
     tx.executeSql('INSERT INTO weights VALUES("182",0,0)');
     tx.executeSql('INSERT INTO weights VALUES("195",0,0)');
     tx.executeSql('INSERT INTO weights VALUES("220",0,0)');
     tx.executeSql('INSERT INTO weights VALUES("285",0,0)');
+});
+
+
+
+//weight template
+db.transaction(function (tx) {
+    tx.executeSql('CREATE TABLE IF NOT EXISTS weightTemplate (weightClass unique, sort)');
+    tx.executeSql('INSERT INTO weightTemplate VALUES("106",0)');
+    tx.executeSql('INSERT INTO weightTemplate VALUES("113",0)');
+    tx.executeSql('INSERT INTO weightTemplate VALUES("120",0)');
+    tx.executeSql('INSERT INTO weightTemplate VALUES("126",0)');
+    tx.executeSql('INSERT INTO weightTemplate VALUES("132",0)');
+    tx.executeSql('INSERT INTO weightTemplate VALUES("138",0)');
+    tx.executeSql('INSERT INTO weightTemplate VALUES("145",0)');
+    tx.executeSql('INSERT INTO weightTemplate VALUES("152",0)');
+    tx.executeSql('INSERT INTO weightTemplate VALUES("160",99)');
+    tx.executeSql('INSERT INTO weightTemplate VALUES("170",0)');
+    tx.executeSql('INSERT INTO weightTemplate VALUES("182",0)');
+    tx.executeSql('INSERT INTO weightTemplate VALUES("195",0)');
+    tx.executeSql('INSERT INTO weightTemplate VALUES("220",0)');
+    tx.executeSql('INSERT INTO weightTemplate VALUES("285",0)');
 });
--- Scripts/DoSomething.js
+++ Scripts/DoSomething.js
@@ -4,11 +4,11 @@
 db.transaction(function (tx) {
     tx.executeSql('SELECT * FROM weights', [], function (tx, results) {
         var len = results.rows.length, i;
-        
+
         for (i = 0; i < len; i++) {
             msg = "<p><b>" + results.rows.item(i).log + "</b></p>";
 
-            var documentIDus= '#us' + results.rows.item(i).weightClass;
+            var documentIDus = '#us' + results.rows.item(i).weightClass;
             var documentIDthem = '#them' + results.rows.item(i).weightClass;
 
             //$(documentID).select(results.rows.item(i).them);
@@ -23,13 +23,110 @@ db.transaction(function (tx) {
         getFinalScore();
     }, null);
 
-   
+
 });
+
+//populate weightclassStart
+db.transaction(function (tx) {
+    tx.executeSql('SELECT * FROM weightTemplate', [], function (tx, results) {
+        var len = results.rows.length, i;
+
+        for (i = 0; i < len; i++) {
+            $('#startingWeightClassSlect').append(new Option(results.rows.item(i).weightClass), results.rows.item(i).weightClass);
+        }
+
+    }, null);
+
+
+});
+function arrangeWeights(event) {
+
+    var seedID;
+    var totalRecords;
+    var updateCTR = 1;
+
+    //grab total # of records in weights tempalte
+    db.transaction(function (tx) {
+        tx.executeSql("select *  from weightTemplate", [], function (tx, results) {
+
+            totalRecords = results.rows.length;
+
+        }, null);
+    });
+
+    //weightTemplate
+    db.transaction(function (tx) {
+        tx.executeSql("update weightTemplate set sort=? where weightClass=?", [0, event.value], function (transaction, result) {
+        }, function (transaction, error) {
+            console.log(error);
+        });
+    }, transError, transSuccess);
+
+    ///get rowID of updated record
+    db.transaction(function (tx) {
+        tx.executeSql("select rowid from weightTemplate where weightClass=?", [event.value], function (tx, results) {
+
+            seedID = results.rows.item(0).rowid;
+
+        }, null);
+    });
+
+
+    ///Update records after
+    db.transaction(function (tx) {
+        //find # records left
+        var offsetRecords = totalRecords - seedID;
+
+        //  tx.executeSql("select weightTemplate LIMIT ? OFFSET ?", [14, seedID], function (tx, results) {
+        tx.executeSql("select weightClass from weightTemplate", [], function (tx, results) {
+            var len = results.rows.length, i;
+
+            for (i = 0; i < len; i++) {
+                var weightToUpdate = results.rows.item(i).weightClass;
+                db.transaction(function (tx) {
+                    tx.executeSql("update weightTemplate set sort=? where weightClass=?", ["1", weightToUpdate], function (transaction, results) {
+                    }, function (transaction, error) {
+                        console.log(error);
+                    });
+                }, transError, transSuccess);
+
+
+                updateCTR++;
+
+            }
+
+        }, null);
+    });
+
+
+
+
+
+    function transError(t, e) {
+        console.log(t);
+        console.log(e);
+        console.error("Error occured ! Code:" + e.code + " Message : " + e.message);
+    }
+
+    function transSuccess(t, r) {
+        console.info("Transaction completed Successfully!");
+        console.log(t);
+        console.log(r);
+    }
+
+};
 
 function clearCache() {
     db.transaction(function (tx) {
         tx.executeSql('DROP TABLE weights');
     });
+
+    db.transaction(function (tx) {
+        tx.executeSql('DROP TABLE weightTemplate');
+    });
+
+
+
 
     window.location.reload(true);
     getFinalScore();
@@ -38,74 +135,107 @@ function clearCache() {
 
 
 function AddDomElem() {
+    db.transaction(function (tx) {
+        tx.executeSql('SELECT * FROM weightTemplate ORDER by sort', [], function (tx, results) {
+            var len = results.rows.length, i;
 
-    var parentNode = $("#TestAppend");
-    
-   
-    var weightClassPanel = document.createElement("div");
-    weightClassPanel.id = "test";
-    weightClassPanel.class = "weightClassPanel";
+            for (i = 0; i < len; i++) {
+                var documentWeightClass = results.rows.item(i).weightClass;
+
+                var parentNode = $("#buildWeightClasses");
+
+                var weightClassPanel = $("<div id='" + documentWeightClass + "'>");
+                weightClassPanel.addClass('weightClassPanel');
+
+                var weightLabellDiv = $("<div>");
+                weightLabellDiv.addClass('weightClassLabel');
+
+                var weightLabel = $("<label>");
+                weightLabel.text(documentWeightClass);
+                weightLabellDiv.append(weightLabel);
+                weightClassPanel.append(weightLabellDiv);
 
 
 
-    var weightLabel = document.createElement("label");
-    weightLabel.appendChild(document.createTextNode("this is my test"));
+                //###################################################################
+                //add weight options us
+                //###################################################################
+                var weightClassOptionsUS = $("<div>");
+                weightClassOptionsUS.addClass('weightClassOptions');
+                var weightClassSelectUS = $("<select id='us" + documentWeightClass + "'>");
 
-    weightClassPanel.append(weightLabel);
+                var weightClassSelect1US = document.createElement("option");
+                weightClassSelect1US.append(new Option("0", "0"));
+
+                var weightClassSelect2US = document.createElement("option");
+                weightClassSelect2US.append(new Option("3", "3"));
+
+                var weightClassSelect3US = document.createElement("option");
+                weightClassSelect3US.append(new Option("4", "4"));
+
+                var weightClassSelect4US = document.createElement("option");
+                weightClassSelect4US.append(new Option("5", "5"));
+
+                var weightClassSelect5US = document.createElement("option");
+                weightClassSelect5US.append(new Option("6", "6"));
+
+                weightClassSelectUS.append(weightClassSelect1US);
+                weightClassSelectUS.append(weightClassSelect2US);
+                weightClassSelectUS.append(weightClassSelect3US);
+                weightClassSelectUS.append(weightClassSelect4US);
+                weightClassSelectUS.append(weightClassSelect5US);
+
+                weightClassOptionsUS.append(weightClassSelectUS);
+                weightClassOptionsUS.attr("onchange", "CalcScore(us" + documentWeightClass + ")");
+                weightClassOptionsUS.addClass("weightSelectUs");
+                //###################################################################
+                //###################################################################
 
 
-    var weightClassOptions = document.createElement("div");
-    weightClassOptions.id = "ustest";
-    weightClassOptions.class = "weightClassPanel";
 
 
-    var weightClassSelect = document.createElement("select");
-    var weightClassSelect1 = document.createElement("option");
-        weightClassSelect1.append(new Option("3", "3"));
-    var weightClassSelect2 = document.createElement("option");
-        weightClassSelect2.append(new Option("4", "4"));
-    var weightClassSelect3 = document.createElement("option");
-        weightClassSelect3.append(new Option("5", "5"));
-    var weightClassSelect4 = document.createElement("option");
-        weightClassSelect4.append(new Option("6", "6"));
+                //###################################################################
+                //add weight options them
+                //###################################################################
+                var weightClassOptionsTHEM = $("<div>");
+                weightClassOptionsTHEM.addClass('weightClassOptions');
+                var weightClassSelectTHEM = $("<select id='them" + documentWeightClass + "'>");
 
-        weightClassSelect.appendChild(weightClassSelect1);
-        weightClassSelect.appendChild(weightClassSelect2);
-        weightClassSelect.appendChild(weightClassSelect3);
-        weightClassSelect.appendChild(weightClassSelect4);
+                var weightClassSelect1THEM = document.createElement("option");
+                weightClassSelect1THEM.append(new Option("0", "0"));
 
-        weightClassOptions.appendChild(weightClassSelect);
+                var weightClassSelect2THEM = document.createElement("option");
+                weightClassSelect2THEM.append(new Option("3", "3"));
 
-    //    < div id= "285" class="weightClassPanel" >
-    //    <div class="weightClassLabel">
-    //        <label>285</label>
-    //    </div>
-    //    <div class="weightClassOptions">
-    //        <select id="us285" onchange="CalcScore(us285)" class="weightSelectUs">
-    //            <option>0</option>
-    //            <option>3</option>
-    //            <option>4</option>
-    //            <option>5</option>
-    //            <option>6</option>
-    //        </select>
-    //    </div>
+                var weightClassSelect3THEM = document.createElement("option");
+                weightClassSelect3THEM.append(new Option("4", "4"));
 
-    //    <div class="weightClassOptions">
-    //        <select id="them285" onchange="CalcScore(them285)" class="weightSelectThem">
-    //            <option>0</option>
-    //            <option>3</option>
-    //            <option>4</option>
-    //            <option>5</option>
-    //            <option>6</option>
-    //        </select>
-    //    </div>
-    //</div>
+                var weightClassSelect4THEM = document.createElement("option");
+                weightClassSelect4THEM.append(new Option("5", "5"));
 
-   // parentNode.appendChild(t);
+                var weightClassSelect5THEM = document.createElement("option");
+                weightClassSelect5THEM.append(new Option("6", "6"));
 
-    parentNode.append(weightClassPanel);
-    parentNode.append(weightClassOptions);
+                weightClassSelectTHEM.append(weightClassSelect1THEM);
+                weightClassSelectTHEM.append(weightClassSelect2THEM);
+                weightClassSelectTHEM.append(weightClassSelect3THEM);
+                weightClassSelectTHEM.append(weightClassSelect4THEM);
+                weightClassSelectTHEM.append(weightClassSelect5THEM);
 
+                weightClassOptionsTHEM.append(weightClassSelectTHEM);
+                weightClassOptionsTHEM.attr("onchange", "CalcScore(them" + documentWeightClass + ")");
+                weightClassOptionsTHEM.addClass("weightSelectThem");
+
+                weightClassPanel.append(weightClassOptionsUS);
+                weightClassPanel.append(weightClassOptionsTHEM);
+
+
+                parentNode.append(weightClassPanel);
+            }
+        }, null);
+
+
+    });
 
 };
 
@@ -124,9 +254,6 @@ function CalcScore(event) {
     else
         usThem = 'them';
 
-
-   
-  
     if (usThem == 'us') {
         db.transaction(function (tx) {
             tx.executeSql('update weights set us =?, them = 0 where weightClass=?', [parseInt(event.value), weight], function (transaction, result) {
@@ -156,7 +283,7 @@ function CalcScore(event) {
         console.log(r);
     }
 
-    
+
     getFinalScore();
 
 };
@@ -222,7 +349,7 @@ function validateBox(eleName) {
 
     var first = eleName.substr(0, 2);
     var weight = eleName.substr(eleName.length - 3);
-    if (first === 'us'){
+    if (first === 'us') {
         //check them to see if there is a value, if there is, 0 it out
         var themElem = '#them' + weight;
         $(themElem).val('0');
@@ -231,6 +358,6 @@ function validateBox(eleName) {
         var usElem = '#us' + weight;
         $(usElem).val('0');
     }
-    
+
 
 };
